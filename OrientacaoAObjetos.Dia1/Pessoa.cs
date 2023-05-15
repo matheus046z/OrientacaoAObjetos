6e0b40fa2cda0e0b34eeb0b40ee8b2de35542387
Adriano Gomes <112133600+Adrianodvs013@users.noreply.github.com>
2023-05-15 16:13:57 -0300

    Commit inicial

---
+++ OrientacaoAObjetos.Dia1/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; // importa namespace
+
+namespace OrientacaoAObjetos.Dia1 // define um name space
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Interesse { get; set; }
+        public bool Socio { get; set; }
+        public decimal Mensalidade { get; set; }    
+
+        public bool VerificarSociedade()
+        {
+            if (Socio)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}

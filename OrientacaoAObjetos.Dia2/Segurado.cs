ce82197cff5ba6bb2c2aeae528d099915eef25e0
Matheus <mt.1_@live.com>
2023-05-16 16:16:09 -0300

    Dia 2

---
+++ OrientacaoAObjetos.Dia2/Segurado.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoAObjetos.Dia2
+{
+    public class Segurado
+    {
+        public int Id { get; set; }
+        public  string Nome { get; set; }
+        public string CPF { get; set; } 
+
+
+        // construtor tambem serve para passar dados
+        // O construtor tem o mesmo nome da classe
+        // 1o construtor -> default (nao definido, padrao, ja existe, vem da orientação a objeto)
+        // 2o construtor -> com parametro
+        // 3o construtor -> com sobrecarga (override), dá opções diferenciadas, ex pesquisar id e nome
+        // O construtor constroi o objeto na memoria
+        // injeção de dependencia
+        //garbage collector (gerenciador de memoria interno)
+        public Segurado() // 2o contrutor vazio
+        {
+
+        }
+        //pesquisar por Id
+        public Segurado(int id)
+        {
+            Id = id;
+        }
+        // construtor com sobrecarga (Override)
+        // cadastrar, insert no banco
+        public Segurado(string nome)
+        {
+            Nome = nome;
+        } 
+        public Segurado(string nome, string cpf)
+        {
+            Nome = nome;
+            CPF = cpf;
+        }
+
+        public string Teste()
+        {
+            return "Chegou Aqui! E não é Brazlandia!";
+        }
+    }
+}
